6444d85a030d07a2cf679360c0a726acb0d7707a
Anna Frolova <froliny@gmail.com>
2016-03-14 20:02:55 +0300

    new

--- cat/cat.c
+++ cat/cat.c
@@ -9,20 +9,20 @@ int main()
 	{ 
 		int c = read(STDIN_FILENO, a, 4096);
 		if (c == 0)
-			{
-				return 0;
-			}
+		{
+			return 0;
+		}
 		if (c < 0)
-			{
-				printf("Wrong read");
-				return -1;		
-			}
+		{
+			printf("Wrong read");
+			return -1;		
+		}
 		int b = write(STDOUT_FILENO, a, c);
 		if (b < 0)
-			{
-                                printf("Wrong write");
-                                return -2;
-                        }
+		{
+                        printf("Wrong write");
+                        return -2;
+                }
 	}
 	return 0;
 }
